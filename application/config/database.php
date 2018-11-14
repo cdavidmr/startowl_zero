0c40a3b707cbb66f43590dbd4fcf2f5b3fa5903c
projet <proyecto>
2018-11-14 15:30:52 -0500

    fixing database

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'   => '',
-	'hostname' => 'pgsql:ec2-107-20-193-206.compute-1.amazonaws.com',
+	'hostname' => 'ec2-107-20-193-206.compute-1.amazonaws.com',
 	'port' => 5432,
 	'username' => 'ubyeplxucdwwyy',
 	'password' => 'cb944f1c1274574317b0ef4d8671aef18f01194f4b7826ee39ddd1c08c0c2784',
