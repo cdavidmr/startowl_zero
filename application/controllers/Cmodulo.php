a45bd41e8dc285accbe50025bf288d8ca1f84700
projet <proyecto>
2018-11-14 13:57:33 -0500

    update 15 -delete session

--- application/controllers/Cmodulo.php
+++ application/controllers/Cmodulo.php
@@ -5,9 +5,6 @@ class Cmodulo extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
-		if (!$this->session->userdata("login")) {
-			redirect(base_url()."cloreg/login");
-		}
         $this->load->view('layout/head');
     }
     
