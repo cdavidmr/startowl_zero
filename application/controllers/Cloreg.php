06de9ddc60ad6792e9731e9d428c697ad978dfa8
cdavidmr <cdavidmr@outlook.com>
2018-11-15 06:09:22 -0500

    fixed session path

--- application/controllers/Cloreg.php
+++ application/controllers/Cloreg.php
@@ -5,6 +5,9 @@ class Cloreg extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
+        if (!$this->session->userdata("login")) {
+            redirect(base_url());
+        }
         $this->load->view('layout/head');
     }
     
