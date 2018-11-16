3e36a1f1dd46a38e93c80ae1994ee386b58d0b95
cdavidmr <cdavidmr@outlook.com>
2018-11-15 23:25:10 -0500

    fixing profile

--- application/controllers/Cprofile.php
+++ application/controllers/Cprofile.php
@@ -8,26 +8,28 @@ class Cprofile extends CI_Controller {
         if (!$this->session->userdata("login")) {
             redirect(base_url());
         } 
-        
+
         $this->load->model("Musuario");   
 
         $codigo = $this->session->userdata("cod_usuario");
-
-			$data  = array(		
-                'usuario_profile' => $this->Musuario->getprofile($codigo),
-            );
         $this->load->view('layout/head');
         $this->load->view('layout/header');
     }
     
     public function profile(){
-            
+
+			$data  = array(		
+                'usuario_profile' => $this->Musuario->getprofile($codigo),
+            );    
         $this->load->view('vuserprofile',$data);
         $this->load->view('layout/footer');
     }
     
     public function editprofile(){
-            
+
+        $data  = array(		
+            'usuario_profile' => $this->Musuario->getprofile($codigo),
+        );            
         $this->load->view('vedituserprofile',$data);
         $this->load->view('layout/footer');
     }
