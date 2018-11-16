ffa8a086f44630ccc0c2a13dc42d9e18ba37add8
cdavidmr <cdavidmr@outlook.com>
2018-11-16 00:12:52 -0500

    fixing profile

--- application/controllers/Cprofile.php
+++ application/controllers/Cprofile.php
@@ -8,29 +8,17 @@ class Cprofile extends CI_Controller {
         if (!$this->session->userdata("login")) {
             redirect(base_url());
         }        
-        
         $this->load->model("Musuario");
-
-        $codigo = $this->session->userdata("cod_usuario");
-        $res = $this->Musuario->getProfile($codigo);
-        
-			$data  = array(		
-                'nombre' => $res -> nom_usuario,
-                'apellido' => $res -> apell_usuario,
-                'nacimiento' => $res -> nacimiento_usuario,
-                'empresa' => $res -> empresa_usuario,
-                'competencia' => $res -> cod_habilidad,
-                'correo' => $res -> email_usuario,
-                'pais' => $res -> cod_pais,
-                'idioma' => $res -> cod_idioma,
-                'telefono' => $res -> tel_usuario,
-            );
-
         $this->load->view('layout/head');
         $this->load->view('layout/header');
     }
     
     public function profile(){
+        $codigo = $this->session->userdata("cod_usuario");
+        
+			$data  = array(		
+                'usuario' => $this->Musuario->getProfile($codigo),
+            );
             
         $this->load->view('vuserprofile',$data);
         $this->load->view('layout/footer');
