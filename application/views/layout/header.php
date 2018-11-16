bd2d5b4fe639db6d079732234216e3e376c5953a
cdavidmr <cdavidmr@outlook.com>
2018-11-15 19:24:59 -0500

    fixing logout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -386,7 +386,7 @@
 "></i></div> <div class="menu-text">Privacidad</div> </a> </li>
                     <li> <a href="#"> <div class="menu-icon"><i class=" fa fa-cogs"></i></div> <div class="menu-text">Configuraciones</div> </a> </li>
                     <li> <a href="#"> <div class="menu-icon"><i class="  fa fa-key"></i></div> <div class="menu-text">Seguridad</div> </a> </li>
-                    <li> <a href="<?php base_url(); ?>cauth/logout"> <div class="menu-icon"><i class=" fa fa-sign-out"></i></div>  <div class="menu-text">Cerrar Sesión</div> </a> </li>
+                    <li> <a href="../<?php base_url(); ?>cauth/logout"> <div class="menu-icon"><i class=" fa fa-sign-out"></i></div>  <div class="menu-text">Cerrar Sesión</div> </a> </li>
                     <li class="line"></li>                
                     <li> <a href="#"> <div class="menu-icon"><i class=" fa fa-question-circle"></i></div> <div class="menu-text">Ayuda</div> </a> </li>
                     <li> <a href="#"> <div class="menu-icon"><i class=" glyphicon glyphicon-bullhorn"></i></div> <div class="menu-text">Reportar un problema</div> </a> </li>              
@@ -1046,7 +1046,7 @@
     <div class="vd_menu vd_navbar-bottom-widget">
         <ul>
             <li>
-                <a href="<?php base_url(); ?>cauth/logout">
+                <a href="../<?php base_url(); ?>cauth/logout">
                     <span class="menu-icon"><i class="fa fa-sign-out"></i></span>          
                     <span class="menu-text">Cerrar Sesión</span>             
                 </a>
