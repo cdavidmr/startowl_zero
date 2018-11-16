774f7a2acc070f24a566315cbb103bcbf914073c
cdavidmr <cdavidmr@outlook.com>
2018-11-15 20:32:24 -0500

    fixing login

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -24,13 +24,14 @@
                 <div class="panel-body">
                   <div class="login-icon entypo-icon"> <i class="icon-key"></i> </div>   
 
+                  <form class="form-horizontal" id="login-form" action="<?php echo base_url(); ?>cauth/login" role="form" method="post">
+
                   <?php if($this->session->set_flashdata("error")):?>
                   <div class="alert alert-danger">
                     <button type="button" class="close" data-dismiss="alert"><i class="icon-cross"></i></button>
                     <span class="vd_alert-icon"><i class="fa fa-exclamation-circle vd_red"></i></span><strong>Oh snap!</strong> Verifica tus datos e intenta ingresar nuevamente. </div>
                   <?php endif; ?>
-
-                  <form class="form-horizontal" id="login-form" action="<?php echo base_url(); ?>cauth/login" role="form" method="post">
+                  
                     <!-- <div class="alert alert-success vd_hidden">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true"><i class="icon-cross"></i></button>
                     <span class="vd_alert-icon"><i class="fa fa-check-circle vd_green"></i></span><strong>Datos Correctos!</strong> </div>                   -->
