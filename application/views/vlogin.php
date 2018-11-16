aab202950d97e41bd35aab05aaabc463a4db4b2c
cdavidmr <cdavidmr@outlook.com>
2018-11-15 20:47:24 -0500

    fixing login

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -25,10 +25,11 @@
                   <div class="login-icon entypo-icon"> <i class="icon-key"></i> </div>   
 
                   <?php if($this->session->set_flashdata("error")):?>
-                  <div class="alert alert-danger">
+                  <h4>CONTRASEÑA ERRONEA</h4>
+                  <!-- <div class="alert alert-danger">
                     <button type="button" class="close" data-dismiss="alert"><i class="icon-cross"></i></button>
                     <span class="vd_alert-icon"><i class="fa fa-exclamation-circle vd_red"></i></span><strong>Oh snap!</strong> Verifica tus datos e intenta ingresar nuevamente. </div>
-                  <?php endif; ?>
+                  <?php endif; ?> -->
 
                   <form class="form-horizontal" id="login-form" action="<?php echo base_url(); ?>cauth/login" role="form" method="post">
                     <!-- <div class="alert alert-success vd_hidden">
