149da264b304095f743dae0bff7dc6bd5b133fd8
cdavidmr <cdavidmr@outlook.com>
2018-11-17 11:53:30 -0500

    fixing homepage cdnjs

--- application/views/layout/landing/footerhome.php
+++ application/views/layout/landing/footerhome.php
@@ -25,8 +25,8 @@
     <!-- jQuery, Bootstrap JS. -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="/resources/indexjs/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.5/popper.min.js"></script>
+    <script src="/resources/indexjs/bootstrap.min.js"></script>
 
     <script>
         $(window).scroll(function() {
