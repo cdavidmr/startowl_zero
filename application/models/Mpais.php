59295939e3a0a91190f4455aff79504cdd1fcbd9
cdavidmr <cdavidmr@outlook.com>
2018-11-16 20:28:26 -0500

    fixing register -pais

--- application/models/Mpais.php
+++ application/models/Mpais.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Mpais extends CI_Model {
 
 	public function getPais(){
-		$this->db->order_by("nom_pais", "desc");
+		$this->db->order_by("nom_pais", "asc");
 		$resultados = $this->db->get("pais");
 		return $resultados->result();
 	}
