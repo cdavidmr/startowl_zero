c7430a99eda4e16ef3992ef88c40aeff5278245b
cdavidmr <cdavidmr@outlook.com>
2018-11-18 00:38:27 -0500

    fixing controllers

--- application/controllers/Cidea.php
+++ application/controllers/Cidea.php
@@ -6,12 +6,12 @@ class Cidea extends CI_Controller {
     public function __construct(){
         parent::__construct();
         if (!$this->session->userdata("login")) {
-			redirect(base_url());
-		}	
+            redirect(base_url());
+        }
         $this->load->view('layout/head');
     }
     
-    public function nuevaidea(){
+    public function index(){
         $this->load->view('layout/header');
         $this->load->view('vnuevaidea');
         $this->load->view('layout/footer');
