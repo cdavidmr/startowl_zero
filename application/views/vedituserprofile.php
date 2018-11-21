4e17ff38c93032316ecefb2a9e9a13b88fc74991
cdavidmr <cdavidmr@outlook.com>
2018-11-21 11:24:03 -0500

    fixing editprofile

--- application/views/vedituserprofile.php
+++ application/views/vedituserprofile.php
@@ -478,7 +478,7 @@
                     </div>
                     <!-- panel-body -->
                     <div class="pd-20 center-block">
-                      <button class="btn vd_btn vd_bg-green col-md-offset-3"><span class="menu-icon"><i class="fa fa-fw fa-check"></i></span> Actualizar</button>
+                      <button class="btn vd_btn vd_bg-green col-md-offset-3" type="submit"><span class="menu-icon"><i class="fa fa-fw fa-check"></i></span> Actualizar</button>
                     </div>
                   </form>
                 </div>
